8bf3df125d16bddd4d4339a1b92feb54119a2950
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2026-04-19 14:20:08 +0800

    delete project

--- Source/MP_CRASH/MP_CRASH.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MP_CRASH.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MP_CRASH, "MP_CRASH" );
-
-DEFINE_LOG_CATEGORY(LogMP_CRASH)
--- Source/MP_CRASH/MP_CRASHGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MP_CRASHGameMode.h"
-
-AMP_CRASHGameMode::AMP_CRASHGameMode()
-{
-	// stub
-}
